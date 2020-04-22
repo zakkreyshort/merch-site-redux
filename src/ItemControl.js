e1785b0a501818be33682144eaaf9e8df685b22c
Benjamin Thom <jaminthom@gmail.com>
2020-04-22 10:30:57 -0700

    update src/ItemControl.js
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

---
+++ src/ItemControl.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import NewItemForm from './NewItemForm.js';
+import ItemList from './ItemList.js';
+
+class ItemControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // fill in later with state info
+    };
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+
+export default ItemControl;
