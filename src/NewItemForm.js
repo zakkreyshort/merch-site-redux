89ee8544b9894b93ad01ba84569a0527e087b0ff
Benjamin Thom <jaminthom@gmail.com>
2020-04-22 10:50:38 -0700

    update src/NewItemForm.js to include a form with a submit event handler
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

--- src/NewItemForm.js
+++ src/NewItemForm.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { v4 } from 'uuid';
+// import PropTypes from 'prop-types';
 
-function NewItemForm(props){
+function NewItemForm(){ // add props
   return (
     <React.Fragment>
-      <form>
-        <h3>this is a form</h3>
+      <form onSubmit={handleNewItemFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='item name' />
+        <input
+          type='text'
+          name='description'
+          placeholder='item description' />
+        <input
+          type='number'
+          name='quantity'
+          placeholder='initial quatity' />
+        <button type='submit'>add item</button>
       </form>
     </React.Fragment>
   );
 }
+
+// NewItemForm.propTypes = {
+//   onNewItemCreation: PropTypes.func
+// }
 
 export default NewItemForm;
