c223e51e74161db6ec8f6ad39f516be413ceef09
Benjamin Thom <jaminthom@gmail.com>
2020-04-22 14:14:52 -0700

    update styling in Header.js and Footer.js components
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Footer(){
+
+  const footerStyles = {
+    backgroundColor: '#1A285A',
+    width: '100%',
+    color: '#f5f5f5',
+    display: 'flex',
+    justifyContent: 'space-around',
+    padding: '2%',
+    position: 'fixed',
+    bottom: '0',
+    left: '0'
+  }
+
+  return(
+    <React.Fragment>
+    <div style={footerStyles}>
+      <h3>Earth<span className="merch">Merch</span></h3>
+      <h4>&copy; Michelle Morin, Benjamin Thom, Brandan Sayarath</h4>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,34 @@
 import React from "react";
 
 function Header(){
+
+  const headerStyles = {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    height: '30vh',
+    backgroundColor: '#7FBC76',
+    width: '100%',
+    color: '#f5f5f5'
+  }
+
   return (
     <React.Fragment>
-      <div className="header">
-        <h1>Earth Day Merch</h1>
+      <div style={headerStyles}>
+        <div className="navigation">
+          <a href="#">Home</a>
+          <a href="#">About Us</a>
+          <a href="#">Partners</a>
+          <a href="#">Products</a>
+        </div>
+        <div class="brand-section">
+          <h1>EarthMerch</h1>
+          <div className="mission-statement">
+            <p>Our mission? To build the world’s largest environmental movement to drive transformative change for people and planet.</p>
+            <p>We take time and care to consider where and how things are made, who makes them and what they are made out of.</p>
+            <p>We support local designers, women, minority and family owned businesses and small makers and manufacturers.</p>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
