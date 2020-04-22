53121665622653b924b5e47f0b2f84529d26d0be
Benjamin Thom <jaminthom@gmail.com>
2020-04-22 10:02:18 -0700

    update src/Item.js
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

---
+++ src/Item.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Item(props){
+  return (
+    <React.Fragment>
+      <p>{props.name}</p>
+      <p>{props.description}</p>
+      <p>{props.quantity}</p>
+      <button type="submit">Buy</button>
+      <button type="submit">Restock</button>
+    </React.Fragment>
+  )
+}
+
+Item.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  quantity: PropTypes.number.isRequired
+}
+
+export default Item;
