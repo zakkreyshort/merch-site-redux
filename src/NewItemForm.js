fe3bc895178753f5dd77a238feb7fa30500695c0
Benjamin Thom <jaminthom@gmail.com>
2020-04-22 10:59:10 -0700

    update src/NewItemForm.js to include event handler function
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

--- src/NewItemForm.js
+++ src/NewItemForm.js
@@ -3,6 +3,14 @@ import { v4 } from 'uuid';
 // import PropTypes from 'prop-types';
 
 function NewItemForm(){ // add props
+
+  function handleNewItemFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.description.value);
+    console.log(event.target.quantity.value);
+  }
+
   return (
     <React.Fragment>
       <form onSubmit={handleNewItemFormSubmission}>
