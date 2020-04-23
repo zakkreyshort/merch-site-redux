98fff28d8540cc4bfc766437822d904ac11c898e
Benjamin Thom <jaminthom@gmail.com>
2020-04-23 11:21:59 -0700

    update src/EditItemForm.js add event handler and function to capture form values
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

--- src/EditItemForm.js
+++ src/EditItemForm.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
 
 function EditItemForm(props){
+  const { item } = props;
+
+  function handleEditItemFormSubmission(event){
+    event.preventDefault();
+    props.onEditItem({
+      name: event.target.name.value,
+      description: event.target.description.value,
+      quantity: event.target.quantity.value,
+      id: item.id
+    });
+  }
+  
   return( 
     <React.Fragment>
       <ReusableForm
+        formSubmissonHandler={handleEditItemFormSubmission}
         buttonText="Update Item Details" />
     </React.Fragment>
   );
+}
+
+EditItemForm.propTypes = {
+  onEditItem: PropTypes.func
 }
 
 export default EditItemForm;
