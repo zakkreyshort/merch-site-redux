b96b92e0211f14bb5ee9a91a772adf81f5252053
Benjamin Thom <jaminthom@gmail.com>
2020-04-23 10:06:44 -0700

    update src/ItemDetail.js to render SelectedTicket
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function ItemDetail(props){
+  const { item } = prop;
+
   retun (
     <React.Fragment>
-      <h1>Item Detail</h1>
+      <h1>{item.name}</h1>
+      <p>{item.description}</p>
+      <p>{item.quantity}</p>
     </React.Fragment>
-  )
+  );
 }
+
+ItemDetail.propTypes = {
+  item: PropTypes.object
+};
 
 export default ItemDetail;
