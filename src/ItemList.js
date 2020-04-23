f27962a3f8a47aaa58fb9272d3023af8d7243532
Benjamin Thom <jaminthom@gmail.com>
2020-04-23 09:54:16 -0700

    src/ItemList.js add new prop type for ItemSelection and update props passed to ItemList
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

--- src/ItemList.js
+++ src/ItemList.js
@@ -5,18 +5,22 @@ import PropTypes from "prop-types";
 function ItemList(props){
   return (
   <React.Fragment>
-      {props.itemList.map((element, index) => 
-        <Item name={element.name}
-          description={element.description}
-          quantity={element.quantity}
-          key={index}/>
+      {props.itemList.map((item) => 
+        <Item 
+          whenItemClicked = {props.onItemSelection}
+          name={item.name}
+          description={item.description}
+          quantity={item.quantity}
+          id={item.id}
+          key={item.id}/>
       )}
     </React.Fragment>
   );
 }
 
 ItemList.propTypes = {
-  itemList: PropTypes.array
+  itemList: PropTypes.array,
+  onItemSelection: PropTypes.func
 }
 
 export default ItemList;
