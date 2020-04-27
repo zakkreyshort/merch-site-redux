0e30978f4ad92db22a6c2a9c325e0a9c6588f8e7
Brandan Sayarath <brandan@brandan.tech>
2020-04-27 14:46:54 -0700

    fail then pass test for form visible reducer.
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Rachel  Schieferstein <violenza@gmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -6,6 +6,6 @@ describe("formVisibleReducer", () => {
     });
 
     test('Should toggle form visibility state to true', () => {
-        expect(formVisibleReducer(false, { type: ""}))
-    })
-})
+        expect(formVisibleReducer(false, { type: "TOGGLE_FORM"})).toEqual(true);
+    });
+});
